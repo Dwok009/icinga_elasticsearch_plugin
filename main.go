6cf99c69cd9410e6595a094af747934389991aa6
Derek Walker <143421885+Dwok009@users.noreply.github.com>
2024-06-10 06:38:20 -0500

    Update main.go

--- main.go
+++ main.go
@@ -1,42 +1,46 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "net/http"
-    "os"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
 )
 
 func main() {
-    var host string
-    var port string
-    var uri string
-    var useSSL bool
+	var host string
+	var port string
+	var uri string
+	var useSSL bool
 
-    flag.StringVar(&host, "H", "localhost", "Elasticsearch host")
-    flag.StringVar(&port, "P", "9200", "Elasticsearch port")
-    flag.StringVar(&uri, "u", "/", "Elasticsearch URI")
-    flag.BoolVar(&useSSL, "ssl", false, "Use SSL")
-    flag.Parse()
+	flag.StringVar(&host, "H", "localhost", "Elasticsearch host")
+	flag.StringVar(&port, "P", "", "Elasticsearch port (default 9200 if not provided)")
+	flag.StringVar(&uri, "u", "/", "Elasticsearch URI")
+	flag.BoolVar(&useSSL, "ssl", false, "Use SSL")
+	flag.Parse()
 
-    protocol := "http"
-    if useSSL {
-        protocol = "https"
-    }
+	if port == "" {
+		port = "9200"  // Default Elasticsearch port
+	}
 
-    url := fmt.Sprintf("%s://%s:%s%s", protocol, host, port, uri)
-    resp, err := http.Get(url)
-    if err != nil {
-        fmt.Println("CRITICAL - Unable to reach Elasticsearch:", err)
-        os.Exit(2)
-    }
-    defer resp.Body.Close()
+	protocol := "http"
+	if useSSL {
+		protocol = "https"
+	}
 
-    if resp.StatusCode == http.StatusOK {
-        fmt.Println("OK - Elasticsearch is available")
-        os.Exit(0)
-    } else {
-        fmt.Printf("CRITICAL - Elasticsearch returned status code %d\n", resp.StatusCode)
-        os.Exit(2)
-    }
+	url := fmt.Sprintf("%s://%s:%s%s", protocol, host, port, uri)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("CRITICAL - Unable to reach Elasticsearch:", err)
+		os.Exit(2)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode == http.StatusOK {
+		fmt.Println("OK - Elasticsearch is available")
+		os.Exit(0)
+	} else {
+		fmt.Printf("CRITICAL - Elasticsearch returned status code %d\n", resp.StatusCode)
+		os.Exit(2)
+	}
 }
